de8d38be551bf09a57fcd497684dc91cea84f229
LTD-otech <luka.dlugosz@ontariotechu.net>
2026-03-26 18:45:17 -0400

    2026-03-26
    Luka. D - Working on adding availability to where the user can choose which movie they are wanting to watch and then choose when they are wanting to watch that movie

--- Final Project Intro to Programming.py
+++ Final Project Intro to Programming.py
@@ -1,3 +1,28 @@
+
+def main(): 
+    while True :
+       
+       
+        while True : 
+        
+            party_sz = int(input("How many people are in your party / group : "))
+            party = PartySize(party_sz)
+            print(party)        # Displaying each member with there name and age after all members have been acounted for   
+            movies = ("Spider Man Homecoming", "Star Wars a New Hope" , "Avengers Endgame" , "Fast & Furious 6" , "Scream")
+            showtimes = (f"12:45pm", "3:15pm", "5:45pm" , "8:15pm" , "10:45pm") 
+            
+            moviesshow = (movies,showtimes)
+    
+    
+            repeat = input("If you would like to continue this repeat this enter (y/n) : ")
+            if repeat != ("y") : 
+                break 
+
+main() 
+
+
+
+
 def PartySize(party_sz) : 
     party = []
     
@@ -7,17 +32,22 @@ def PartySize(party_sz) :
         person = (name, age )    
         party.append(person)
     
+    
     return party
 
 
-def main(): 
-        while True : 
-        
-            party_sz = int(input("How many people are in your party / group : "))
-            party = PartySize(party_sz)
-            print(party)        # Displaying each member with there name and age after all members have been acounted for
+def pickingmovieshowtime(movieshow):
+    movies = ("Spider Man Homecoming", "Star Wars the Last Jedi " , "Avengers Endgame" , "Fast & Furious 6" , "Scream " , "Zootopia 2")
+    movies1 = ("Spider Man Homecoming", "Avengers Endgame ", "Scream ")
+    movies2 = (" Star Wars the Last Jedi" , "Fast and Furious 6 " , "Zootopia 2 ")
+    showtimes = ("11:30am","1:45pm", "4:15pm", "6:45pm" , "9:15pm" , "11:45pm") 
+    showtimes1 = ("11:30am", "4:15pm", "9:15pm")
+    showtimes2 = ("1:45am", "6:45pm", "11:45pm")
     
-            repeat = input("If you would like to continue this repeat this enter (y/n) : ")
-            if repeat != ("y") : 
-                break 
-main()
+    
+    print("This is what movies are currently playing : ", movies)
+    pickingmovie = int(input("Pick a number between 0-4 to make your selection of movie : " ))
+    
+     
+    
+    
--- README.md
+++ README.md
@@ -18,3 +18,12 @@ Goals / Main Functions to accomplish :
   -  Bundle 2: Hotdog, Drink 
   -  Bundle 3: Pizza, Fries, Drink
 -  Possibly have it where the seats are stored in a dictionary or file, where, when a different customer (user) wants to use the booking system, the already booked seats will show as taken or unavailable to purchase 
+
+
+All Movies : movies = ("Spider Man Homecoming", "Star Wars the Last Jedi " , "Avengers Endgame" , "Fast & Furious 6" , "Scream " , "Zootopia 2")
+
+All showtimes : 11;30am , 1:45pm , 4:15pm , 6:45pm , 9:15pm , 11:45pm 
+
+Completed / What is currently working : 
+  - Asking the user how many people are in there party/group
+  - Asking the user the name and age of each person inside the party/group 
